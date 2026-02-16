b9e0e80af18877b656333434262d0e939f2aa6bf
xiakezhu <xiakezhu@usc.edu>
2026-02-15 21:24:14 -0800

    Add nvim config

---
+++ .config/nvim/lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+-- ~/.config/nvim/lua/custom/plugins/neo-tree.lua
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+  end,
+  lazy = false,
+}
